03cf1fae63b42478215c7670e9ea05cb0b975c6e
SrijanCh <srijancherupally@gmail.com>
2026-07-16 14:06:17 -0700

    dps368 i2c  works now

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,18 @@ RGBLed led(24,23,22);
 
 void setup() {
     baro.begin();
-    led.setColor(0.0f, 0.5882352941f, 1.0f); // Bright Blue
     Serial.println(baro.isConnected());
 }
 
 void loop() {
     Serial.println(baro.getAltitudeCm());
     baro.update();
+}
+
+void setup1() {
+    led.setColor(0.0f, 0.5882352941f, 1.0f); // Bright Blue
+}
+
+void loop1() {
     led.rainbow(10);
 }
